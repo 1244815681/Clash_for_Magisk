7f46065ecb1291b94abc244542e0039af20d44c3
Tamarin <71506581+taamarin@users.noreply.github.com>
2022-01-30 03:08:15 +0700

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,7 @@
 Clash_data_dir="/data/adb/clash"
-Clash_sc_dir="/data/adb/service.d"
-Clash_ui_dir="/data"
 
 rm_data() {
     rm -rf ${Clash_data_dir}
-    rm -rf ${Clash_ui_dir}/file.php
-    rm -rf ${Clash_ui_dir}/index.php
-    rm -rf ${Clash_ui_dir}/index.html
-    rm -rf ${Clash_ui_dir}/bg.png
-    rm -rf ${Clash_ui_dir}/saklar.php
-    rm -rf ${Clash_sc_dir}/start_php.sh
-    rm -rf ${Clash_sc_dir}/ClashforMagisk_service.sh
-
 }
 
 rm_data
